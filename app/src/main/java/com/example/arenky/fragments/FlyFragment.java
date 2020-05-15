82f8ee2b81d8ab7d67e808aeb984a87ce5dd3baf
Luis Cruz <luisccc99@gmail.com>
2020-05-15 13:19:10 -0600

    agregando campos en fly fragment e implementando algunos paquetes para las APIs

--- app/src/main/java/com/example/arenky/fragments/FlyFragment.java
+++ app/src/main/java/com/example/arenky/fragments/FlyFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.arenky.R;
 
@@ -14,6 +17,12 @@ import com.example.arenky.R;
  * A simple {@link Fragment} subclass.
  */
 public class FlyFragment extends Fragment {
+
+    Button btnBuscar;
+    EditText txtOrigen;
+    EditText txtDestino;
+
+    TextView txtPrueba;
 
     public FlyFragment() {
         // Required empty public constructor
@@ -24,6 +33,18 @@ public class FlyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fly, container, false);
+        View view = inflater.inflate(R.layout.fragment_fly, container, false);
+        btnBuscar = view.findViewById(R.id.btnBuscar);
+        txtOrigen = view.findViewById(R.id.txtOrigen);
+        txtDestino = view.findViewById(R.id.txtDestino);
+        txtPrueba = view.findViewById(R.id.txtPrueba);
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtPrueba.setText(txtOrigen.getText() + ", " + txtDestino.getText());
+            }
+        });
+        return view;
     }
 }
